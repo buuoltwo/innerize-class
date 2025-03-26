0b63c1a7c34949d1d95c948d888ac115c230698d
buuoltwo <bullnick20@gmail.com>
2025-03-27 01:19:15 +0800

    匿名内部类 Lambda表达式 Stream

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -1,7 +1,10 @@
 package com.github.hcsp.polymorphism;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class User {
     /** 用户ID，数据库主键，全局唯一 */
@@ -26,9 +29,36 @@ public class User {
     // 这里使用了一个NameCollector类，请尝试将它改写成匿名内部类
     // 使得代码更加集中，更加容易阅读
     public static List<String> collectNames(List<User> users) {
-        NameCollector collector = new NameCollector();
-        users.forEach(collector);
-        return collector.getNames();
+//        NameCollector collector = new NameCollector();
+//        users.forEach(collector);
+//        return collector.getNames();
+
+//        List names = new ArrayList();
+//        users.forEach(new Consumer<User>() {
+//
+//            @Override
+//            public void accept(User user) {
+//                names.add(user.getName());
+//            }
+//
+//            public List<String> getNames() {
+//                return names;
+//            }
+//        });
+//        return names;
+//
+
+//        Lambda表达式
+//        List names = new ArrayList();
+//        users.forEach(user -> names.add(user.getName()));
+//        return names;
+
+
+        // 或者使用Stream API（推荐）
+        return users
+            .stream()
+            .map(User::getName)
+            .collect(Collectors.toList());
     }
 
     public static void main(String[] args) {
